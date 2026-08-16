95882392bbbfbdb447347a69fdd6ca8bd2881ac6
your username <sccnha@gmail.com>
2026-08-17 04:11:19 +0700

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:'/BuildTrust/',
-  server: {
-    port: 5173,
-  },
+  base:"/BuildTrust/",
 });
